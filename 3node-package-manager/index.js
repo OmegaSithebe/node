06343e0cab17e7cb3acfb96bc3fbd67553ae721f
OmegaSithebe <rogersithebe@gmail.com>
2025-04-22 20:34:52 +0200

    22/04/25 home edit

--- 3node-package-manager/index.js
+++ 3node-package-manager/index.js
@@ -2,7 +2,7 @@
 const lodash = require('lodash');
 
 const names = ['john', 'lilly', 'siphiwe', 'sipho', 'tim'];
-const capatalize = lodash.map(names, lodash.capatalize);
+const capatalize = lodash.map(names, lodash.capitalize);
 
 console.log(capatalize);
 
